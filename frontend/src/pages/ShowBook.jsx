5623bc57a6b74b34c840d32621b2220e764d3de3
Heshan-w <htwanigasinghe@gmail.com>
2024-03-20 01:46:52 +0530

    style: updated font colours and sizes

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -29,35 +29,31 @@ const ShowBook = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4 m-7">
+        <div className="flex flex-col border-4 border-sky-800 rounded-xl w-fit p-4 m-7">
           <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Book ID : </span>
+            <span className="text-2xl mr-4">Book ID : </span>
             <span>{book._id}</span>
           </div>
           <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Title : </span>
+            <span className="text-2xl mr-4">Title : </span>
             <span>{book.title}</span>
           </div>
           <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">Author : </span>
+            <span className="text-2xl mr-4">Author : </span>
             <span>{book.author}</span>
           </div>
 
           <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">
-              Year of publication :
-            </span>
+            <span className="text-2xl mr-4">Year of publication :</span>
             <span>{book.publishYear}</span>
           </div>
           <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">
-              Book added to library on :
-            </span>
+            <span className="text-2xl mr-4">Book added to library on :</span>
             <span>{new Date(book.createdAt).toString()}</span>
           </div>
 
           <div className="my-4">
-            <span className="text-xl mr-4 text-gray-500">
+            <span className="text-2xl mr-4">
               Book last updated on :
             </span>
             <span>{new Date(book.updatedAt).toString()}</span>
