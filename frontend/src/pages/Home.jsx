fb98eacf8557187f27309c28255e9320d46d4913
Heshan-w <htwanigasinghe@gmail.com>
2024-03-20 14:35:03 +0530

    Add dotenv package and update dependencies

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,12 +5,15 @@ import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import BooksTable from "../component/home/BooksTable";
+import BooksCard from "../component/home/Bookscard";
 
 const Home = () => {
   // books is a state variable that will store the list of books.
   const [books, setBooks] = useState([]);
   // loading is a state variable that will be used to show a loading spinner when the books are being fetched from the server.
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
 
   // The useEffect hook is used to fetch the list of books from the server when the component is first rendered.
   useEffect(() => {
@@ -33,7 +36,21 @@ const Home = () => {
     <div className="p-4">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-5xl my-5">Bookshelf</h1>
-        <div className="flex flex-col justify-center items-center mb-5">
+        <div className="flex justify-center items-center gap-x-4">
+          <button
+            className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+            onClick={() => setShowType("table")}
+          >
+            Table View
+          </button>
+          <button
+            className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+            onClick={() => setShowType("card")}
+          >
+            Card View
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center my-5">
           <Link to={"/books/create"}>
             <MdOutlineAddBox className="text-sky-800 text-4xl" />
           </Link>
@@ -43,66 +60,10 @@ const Home = () => {
       {/* if loading is not true, meaning the data has been fetched, a table will be displayed with the list of books. */}
       {loading ? (
         <Spinner />
+      ) : showType === "table" ? (
+        <BooksTable books={books} />
       ) : (
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
-            <tr>
-              <th className="border border-slate-600 rounded-md bg-slate-600 text-gray-400">
-                No.
-              </th>
-              <th className="border border-slate-600 rounded-md bg-slate-600 text-gray-400">
-                Title
-              </th>
-              <th className="border border-slate-600 rounded-md max-md:hidden bg-slate-600 text-gray-400">
-                Author
-              </th>
-              <th className="border border-slate-600 rounded-md max-md:hidden bg-slate-600 text-gray-400">
-                Publish Year
-              </th>
-              <th className="border border-slate-600 rounded-md bg-slate-600 text-gray-400">
-                Operations
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* The books state variable is mapped to create a table row for each book. */}
-            {/* each object in the books arrya has the properties _id, title, author, and publishYear. */}
-            {books.map((book, index) => {
-              return (
-                <tr key={book._id} className="h-8">
-                  <td className="border border-slate-600 rounded-md text-center">
-                    {/* The index is used to display the number of the book in the list. */}
-                    {index + 1}
-                  </td>
-                  <td className="border border-slate-600 rounded-md text-center">
-                    {book.title}
-                  </td>
-                  <td className="border border-slate-600 rounded-md max-md:hidden text-center">
-                    {book.author}
-                  </td>
-                  <td className="border border-slate-600 rounded-md max-md:hidden text-center">
-                    {book.publishYear}
-                  </td>
-                  <td className="border border-slate-600 rounded-md text-center">
-                    <div className="flex justify-center gap-x-4">
-                      <Link to={`/books/details/${book._id}`}>
-                        <BsInfoCircle className="text-2xl text-green-800" />
-                      </Link>
-
-                      <Link to={`/books/edit/${book._id}`}>
-                        <AiOutlineEdit className="text-2xl text-yellow-400" />
-                      </Link>
-
-                      <Link to={`/books/delete/${book._id}`}>
-                        <MdOutlineDelete className="text-2xl text-red-600" />
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <BooksCard books={books} />
       )}
     </div>
   );
